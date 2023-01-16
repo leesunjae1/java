cf7d1317f37e1fb77b80440e455b8aaf30b8d578
이선재 <1019lsj@naver.com>
2023-01-16 13:43:30 +0900

    23.01.16

--- 이선재.hr/src/com/my/hr/Main.java
+++ 이선재.hr/src/com/my/hr/Main.java
@@ -1,14 +1,25 @@
 package com.my.hr;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.my.hr.dao.LaborerDao;
+import com.my.hr.dao.LaborerDaoImpl;
+import com.my.hr.domain.Laborer;
+import com.my.hr.presentation.Console;
+import com.my.hr.presentation.LaborerIo;
+import com.my.hr.service.LaborerService;
+import com.my.hr.service.LaborerServiceImpl;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-	
+		List<Laborer> laborers = new ArrayList<>();
 		
-	
+		LaborerDao laborerDao = new LaborerDaoImpl(laborers);
+		LaborerService laborerService = new LaborerServiceImpl(laborerDao);
+		LaborerIo laborerIo = new LaborerIo(laborerService);
 		
-	
+		laborerIo.play();
+		Console.info("end.");
 	}
 }
