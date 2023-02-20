2944b3ce1db93bd99f235a3eabf9fa49638be348
'sunjae' <1019lsj@naver.com>
2023-02-20 18:28:05 +0900

    23.02.20

--- servlet/src/main/java/ch01/home/GetPost.java
+++ servlet/src/main/java/ch01/home/GetPost.java
@@ -11,28 +11,16 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/ch01/home/add")
 public class GetPost extends HttpServlet{
-	
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse res) 
+	public void service(HttpServletRequest req, HttpServletResponse res)
 		throws ServletException, IOException {
 		int num1 = 1;
 		int num2 = 2;
 		
 		res.setContentType("text/html; charset=utf-8");
 		PrintWriter out = res.getWriter();
-		out.println("<h2>GET</h2>");
-		out.printf("<p>%d + %d = %d</p>", num1, num2, num1 + num2);
+		out.println("<h2>" + req.getMethod() + "</h2>");
+		out.printf("<p>%d + %d = %d<p>", num1, num2, num1 + num2);
 	}
 	
-	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse res) 
-		throws ServletException, IOException{
-		int num1 = 1;
-		int num2 = 2;
-		
-		res.setContentType("text/html; charset=utf-8");
-		PrintWriter out = res.getWriter();
-		out.println("<h2>POST</h2>");
-		out.printf("<p>%d + %d = %d</p>", num1, num2, num1 + num2);
-	}
 }
